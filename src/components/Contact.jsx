d7f43f85962f72ceacc2f3da26d986fb457683a3
Akansha Pandey <124514156+Akansha2004pandey@users.noreply.github.com>
2024-07-11 09:08:29 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { useRef } from 'react';
-import { forwardRef } from 'react';
-const Contact =forwardRef((props,ref) => {
-  
-  
+import React, { forwardRef } from 'react';
+
+const Contact = forwardRef((props, ref) => {
   return (
     <div id="contact" ref={ref} className='flex justify-center'>
-        <h4 className='m-4 font-md text-xl py-2 mb-10 px-10 border-[2px] border-gray-200 text-gray-500 rounded-lg'>akanshaoptimist@gmail.com</h4>
+      <a 
+        href="mailto:akanshaoptimist@gmail.com?subject=Contact Us&body=Email: akanshaoptimist@gmail.com" 
+        className='m-4 font-md text-xl py-2 mb-10 px-10 border-[2px] border-gray-200 text-gray-500 rounded-lg cursor-pointer'
+      >
+        akanshaoptimist@gmail.com
+      </a>
     </div>
+  );
+});
 
-  )
-}
-)
-export default Contact
+export default Contact;
